9c21ebce3210d8b856d370205b8d8092e7e72e15
ayato <ayato-shitomi>
2023-03-29 21:20:50 +0900

    2023年  3月 29日 水曜日 21:20:50 JST

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,17 +1,23 @@
+/*
+void overflow_handler(int signal) {
+    if (signal == SIGABRT) {
+        std::cerr << "Error: Integer overflow detected." << std::endl;
+    } else {
+        std::cerr << "Error: Some error." << std::endl;
+    }
+    std::exit(1);
+}
+
+    // check for overflow
+    std::signal(SIGABRT, overflow_handler);
+*/
 
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <stack>
-#include <sstream>
-#include <vector>
 #include <cstdlib>
-#include <iostream>
 #include <csignal>
-#include <cstdlib>
-
-bool is_operator(const std::string &token) {
-    return token == "+" || token == "-" || token == "*" || token == "/";
-}
 
 void overflow_handler(int signal) {
     if (signal == SIGABRT) {
@@ -22,70 +28,63 @@ void overflow_handler(int signal) {
     std::exit(1);
 }
 
-int perform_operation(const std::string &op, int a, int b) {
-    if (op == "+") return a + b;
-    if (op == "-") return a - b;
-    if (op == "*") return a * b;
-    if (op == "/") return a / b;
-    return 0;
+bool is_operator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/');
 }
 
-std::vector<std::string> tokenize(const std::string &expr) {
-    std::istringstream iss(expr);
-    std::vector<std::string> tokens;
-    std::string token;
-
-    while (iss >> token) {
-        tokens.push_back(token);
+int perform_operation(char op, int a, int b) {
+    switch (op) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            if (b == 0) {
+                throw std::runtime_error("ERROR");
+            }
+            return a / b;
+        default:
+            throw std::runtime_error("ERROR");
     }
-
-    return tokens;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     if (argc != 2) {
-        std::cerr << "Error" << std::endl;
+        std::cerr << "ERROR" << std::endl;
         return 1;
     }
-
     std::signal(SIGABRT, overflow_handler);
-
-    std::string input_expr(argv[1]);
-    std::vector<std::string> tokens = tokenize(input_expr);
+    std::istringstream iss(argv[1]);
+    std::string token;
     std::stack<int> operands;
 
-    for (std::vector<std::string>::const_iterator it = tokens.begin(); it != tokens.end(); ++it) {
-        const std::string &token = *it;
-        if (is_operator(token)) {
+    while (iss >> token) {
+        if (is_operator(token[0])) {
             if (operands.size() < 2) {
-                std::cerr << "Error" << std::endl;
+                std::cerr << "ERROR" << std::endl;
                 return 1;
             }
-
             int b = operands.top(); operands.pop();
             int a = operands.top(); operands.pop();
-            int result = perform_operation(token, a, b);
-
+            int result = perform_operation(token[0], a, b);
             operands.push(result);
         } else {
-            int number = std::atoi(token.c_str());
-
-            if (number < 0 || number >= 10) {
-                std::cerr << "Error" << std::endl;
+            int num = std::atoi(token.c_str());
+            if (num < 0 || num >= 10) {
+                std::cerr << "ERROR" << std::endl;
                 return 1;
             }
-
-            operands.push(number);
+            operands.push(num);
         }
     }
 
     if (operands.size() != 1) {
-        std::cerr << "Error" << std::endl;
+        std::cerr << "ERROR" << std::endl;
         return 1;
     }
 
     std::cout << operands.top() << std::endl;
     return 0;
 }
-
-
