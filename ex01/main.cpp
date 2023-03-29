2e1db4ea5ffbb4dda69c8e0717c819d0a38bc6ed
ayato <ayato-shitomi>
2023-03-29 19:42:36 +0900

    2023年  3月 29日 水曜日 19:42:36 JST

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -24,28 +24,25 @@ bool isNumeric(const std::string& str) {
     return true;
 }
 
-
-bool chkNumbers(int num, std::stack<std::string> tokens) {
-	std::string str = tokens.top();
-	tokens.pop();
-
+bool chkNumbers(int num, std::string str) {
+	/*
 	std::cout << "num:	" << num << std::endl;
 	std::cout << "str:	" << str << std::endl;
+	*/
 
-    // 数値を文字列に変換
+    // convert int to string
     std::string numStr = "";
     while (num > 0) {
         char c = '0' + num % 10;
         numStr = c + numStr;
         num /= 10;
     }
-    // 文字列と1文字ずつ比較
+    // compare the strings
     for (size_t i = 0; i < numStr.length(); i++) {
-        if (i >= str.length() || numStr[i] != str[i]) {
+        if (i >= str.length() || numStr[i] != str[i])
             return false;
-        }
     }
-    // 文字列の長さもチェック
+    // check if the string is longer than the number
     return numStr.length() == str.length();
 }
 
@@ -58,8 +55,6 @@ int main(int ac, char* av[]) {
 	std::istringstream arg(av[1]);
 	std::istringstream chkArg(av[1]);
 	std::string token;
-
-	// std::cout << (av[1]) << std::endl;
 	
 	size_t n = 0;
 	size_t spaceCount = 1;
@@ -72,28 +67,32 @@ int main(int ac, char* av[]) {
 	size_t count = 0;
 	std::string tokenTmp;
 	std::stack<std::string> tokens;
-	std::cout << "====" << std::endl;
+	// std::cout << "====" << std::endl;
 	while (chkArg >> token) {
-		std::cout << "token:	" << token << std::endl;
+		// std::cout << "token:	" << token << std::endl;
 		tokenTmp += token + " ";
 		if (isNumeric(token))
 			tokens.push(token);
 		count++;
 	}
 
-	std::cout << "====" << std::endl;
+	// std::cout << "====" << std::endl;
+	/*
 	while (!tokens.empty()) {
         std::cout << "stacks:	" << tokens.top() << std::endl;
         tokens.pop();
     }
+	*/
+	/*
 	std::cout << "token tmp :	" << tokenTmp << std::endl;
 	std::cout << "argment   :	" << av[1] << std::endl;
 	std::cout << "token count: " << count << "\n" << "space count: " << spaceCount << std::endl;
+	*/
 	if (count != spaceCount)
 		putError("Over or under flow detected.");
 	// get the tokens from the argument
-	std::cout << "" << std::endl;
-	std::cout << "" << std::endl;
+	// std::cout << "" << std::endl;
+	// std::cout << "" << std::endl;
 	while (arg >> token) {
 		if (token == "+" || token == "-" || token == "*" || token == "/") {
 			
@@ -101,37 +100,52 @@ int main(int ac, char* av[]) {
 			if (numbers.size() < 2)
 				putError("Insufficient operands for '" + token + "' operator.");
 			
-			std::cout << "second:	" << numbers.top() << std::endl;
+			// std::cout << "second:	" << numbers.top() << std::endl;
 			int secondNum = numbers.top(); numbers.pop();
-			if (!(chkNumbers(secondNum, tokens)))
-				putError("Over or under flow detected with stack cast. 2nd");
-			std::cout << "first:	" << numbers.top() << std::endl;
+			if (!(chkNumbers(secondNum, tokens.top())))
+				putError("Over or under flow detected: " + tokens.top());
+			tokens.pop();
+			// std::cout << "first:	" << numbers.top() << std::endl;
 			int firstNum = numbers.top(); numbers.pop();
-			if (!(chkNumbers(firstNum, tokens)))
-				putError("Over or under flow detected with stack cast. 1st");
+			if (!(chkNumbers(firstNum, tokens.top())))
+				putError("Over or under flow detected: " + tokens.top());
+			tokens.pop();
 			int res;
 			if (secondNum < 0 || firstNum < 0)
 				putError("Bad number was given.");
 
-			std::cout << "firstNum: " << firstNum << std::endl;
-			std::cout << "secondNum: " << secondNum << std::endl;
+			// std::cout << "firstNum: " << firstNum << std::endl;
+			// std::cout << "secondNum: " << secondNum << std::endl;
 
 			// check the operator and calculate
 			if (token == "+") {
+				// check if the result is overflow or underflow
 				if (secondNum > 0 && firstNum > INT_MAX - secondNum)
 					putFlow(firstNum, secondNum, token);
 				else if (secondNum < 0 && firstNum < INT_MIN - secondNum)
 					putFlow(firstNum, secondNum, token);
 				res = firstNum + secondNum;
-			}
-			else if (token == "-")
+			} else if (token == "-") {
+				// check if the result is overflow or underflow
+				if (secondNum > 0 && firstNum < INT_MIN + secondNum)
+					putFlow(firstNum, secondNum, token);
+				else if (secondNum < 0 && firstNum > INT_MAX + secondNum)
+					putFlow(firstNum, secondNum, token);
 				res = firstNum - secondNum;
-			else if (token == "*")
+			} else if (token == "*") {
+				// check if the result is overflow or underflow
+				if (secondNum > 0 && firstNum > INT_MAX / secondNum)
+					putFlow(firstNum, secondNum, token);
+				else if (secondNum < 0 && firstNum < INT_MIN / secondNum)
+					putFlow(firstNum, secondNum, token);
 				res = firstNum * secondNum;
-			else if (token == "/") {
+			} else if (token == "/") {
 				// check if the second number is zero
 				if (secondNum == 0)
 					putError("Division by zero.");
+				// check if the result is overflow or underflow
+				if (secondNum == -1 && firstNum == INT_MIN)
+					putFlow(firstNum, secondNum, token);
 				res = firstNum / secondNum;
 			}
 			// push the result to the stack
