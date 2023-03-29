92adab5e384bee839ca011f4ebcee338c2fd6c35
ayato <ayato-shitomi>
2023-03-29 21:28:02 +0900

    2023年  3月 29日 水曜日 21:28:02 JST

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,16 +1,3 @@
-/*
-void overflow_handler(int signal) {
-    if (signal == SIGABRT) {
-        std::cerr << "Error: Integer overflow detected." << std::endl;
-    } else {
-        std::cerr << "Error: Some error." << std::endl;
-    }
-    std::exit(1);
-}
-
-    // check for overflow
-    std::signal(SIGABRT, overflow_handler);
-*/
 
 #include <iostream>
 #include <sstream>
@@ -19,6 +6,7 @@ void overflow_handler(int signal) {
 #include <cstdlib>
 #include <csignal>
 
+// print error message and exit
 void overflow_handler(int signal) {
     if (signal == SIGABRT) {
         std::cerr << "Error: Integer overflow detected." << std::endl;
@@ -28,10 +16,12 @@ void overflow_handler(int signal) {
     std::exit(1);
 }
 
+// check if character is an operator
 bool is_operator(char c) {
     return (c == '+' || c == '-' || c == '*' || c == '/');
 }
 
+// perform operation
 int perform_operation(char op, int a, int b) {
     switch (op) {
         case '+':
@@ -55,6 +45,7 @@ int main(int argc, char* argv[]) {
         std::cerr << "ERROR" << std::endl;
         return 1;
     }
+    // check for overflow
     std::signal(SIGABRT, overflow_handler);
     std::istringstream iss(argv[1]);
     std::string token;
@@ -62,16 +53,21 @@ int main(int argc, char* argv[]) {
 
     while (iss >> token) {
         if (is_operator(token[0])) {
+            // check if there are at least two operands
             if (operands.size() < 2) {
                 std::cerr << "ERROR" << std::endl;
                 return 1;
             }
+            // pop two operands
             int b = operands.top(); operands.pop();
             int a = operands.top(); operands.pop();
             int result = perform_operation(token[0], a, b);
+            // check for overflow
             operands.push(result);
         } else {
+            // convert token to number
             int num = std::atoi(token.c_str());
+            // check if token is a number
             if (num < 0 || num >= 10) {
                 std::cerr << "ERROR" << std::endl;
                 return 1;
@@ -80,11 +76,13 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    // check if there is only one operand left
     if (operands.size() != 1) {
         std::cerr << "ERROR" << std::endl;
         return 1;
     }
 
+    // print result
     std::cout << operands.top() << std::endl;
     return 0;
 }
